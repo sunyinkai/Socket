de3a9f62e67d018b8e669e021fc23e4936d39198
sunyinkai <1091491336@qq.com>
2018-11-05 13:05:44 +0800

    0.1

--- debug.py
+++
@@ -1,7 +0,0 @@
-# data=b"58\n1\nsunyinkai\n123456"
-# lenth, command, username, passwd = str(data).split('\\n')
-# print(lenth, command, username, passwd)
-data="你好"
-
-print(data.encode())
-print(data.encode().decode())
