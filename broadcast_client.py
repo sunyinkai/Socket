fa9757f466cc9c97ac472ee84309a9cb47cb48dd
sunyinkai <1091491336@qq.com>
2018-11-27 23:10:39 +0800

    broadcast simple1.1

--- broadcast_client.py
+++ broadcast_client.py
@@ -2,12 +2,14 @@ import socket
 
 
 def connect():
-    address=('127.0.0.1',12345)
-    s=socket.socket()
+    address = ('127.0.0.1', 12345)
+    s = socket.socket()
     s.connect(address)
     while True:
-        d=s.recv(1024)
-        with open("out.txt","a") as f:
+        d = s.recv(1024)
+        with open("out.txt", "a") as f:
             f.write(d.decode())
-if __name__ =='__main__':
+
+
+if __name__ == '__main__':
     connect()
--- broadcast_server.py
+++ broadcast_server.py
@@ -1,49 +1,51 @@
 import asyncio
 import socket
-import time
 from random import randint
 
-all_cnt=0 # 目前服务器已经生产了多少个字节
+all_cnt = 0  # 目前服务器已经生产了多少个字节的数据
 
 
 async def generate_num(timer=0.25):
     global all_cnt
     while True:
-        with open('in.txt','a') as f:
-            val=randint(0,100)
-            string=str(val)+'\n'
+        with open('in.txt', 'a') as f:
+            val = randint(0, 100)
+            string = str(val) + '\n'
             f.write(string)
-            all_cnt=all_cnt+len(string)
+            all_cnt = all_cnt + len(string)
         await asyncio.sleep(timer)  # 挂起当前协程
 
 
-async def send_num(sock,address):
-    print('accept a connection from %s:%s'%(address))
-    now_cnt=0  # 当前sock已经接收到了什么位置
+async def send_num(sock, address):
+    print('accept a connection from %s:%s' % (address))
+    now_cnt = 0  # 当前sock已经接收到了什么位置
     while True:
-        with open('in.txt','r') as f:
+        num = ''
+        with open('in.txt', 'r') as f:
             if now_cnt < all_cnt:
                 f.seek(now_cnt)
-                num=f.readline()
-                await loop.sock_sendall(sock,num.encode())
-                now_cnt=now_cnt+len(num)
-            else:
-                await asyncio.sleep(0.001)
+                num = f.readline()
+        if len(num):
+            await loop.sock_sendall(sock, num.encode())
+            now_cnt = now_cnt + len(num)
+        else:
+            await asyncio.sleep(0.0001)   # 挂起当前协程
 
 
 async def main():
-    address=('0.0.0.0',12345)
-    s=socket.socket()
+    address = ('0.0.0.0', 12345)
+    s = socket.socket()
     s.bind(address)
     s.listen(5)
     s.setblocking(False)
     print('the server is start!')
     while True:
-        sock,address=await loop.sock_accept(s)
-        loop.create_task(send_num(sock,address))
+        sock, address = await loop.sock_accept(s)
+        loop.create_task(send_num(sock, address))
 
-if __name__=='__main__':
-    loop=asyncio.get_event_loop()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
     loop.create_task(main())
     loop.create_task(generate_num())
     loop.run_forever()
